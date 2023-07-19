19098af335451eb267c01c242f5b88f44a4e3475
KimSkade <uuebm@student.kit.edu>
2023-07-19 13:06:14 +0200

    QualityModel

---
+++ txtEinlesen.py
@@ -0,0 +1,38 @@
+def getValuefromColumName(dateipfad, spaltenname, zeilennummer):
+    try:
+        with open(dateipfad, 'r') as file:
+            spaltenIndices = {}  # Dictionary zur Speicherung der Spaltenindices
+            spaltennamen = None
+
+            aktuelleZeilennummer = 0
+
+            for zeile in file:
+                spalten = zeile.strip().split('\t')
+
+                if aktuelleZeilennummer == 0:
+                    # Spaltennamen in Zeile 0 speichern
+                    spaltennamen = spalten
+                    for i in range(len(spaltennamen)):
+                        spaltenIndices[spaltennamen[i]] = i
+                elif aktuelleZeilennummer == zeilennummer:
+                    # Gewünschte Zeile gefunden
+                    spaltenindex = spaltenIndices.get(spaltenname)
+                    if spaltenindex is not None and spaltenindex < len(spalten):
+                        return spalten[spaltenindex]
+
+                aktuelleZeilennummer += 1
+
+            if spaltennamen is None:
+                print("Spaltennamen nicht gefunden")
+    except IOError as e:
+        print("Ein Fehler ist beim Lesen der Datei aufgetreten:", e)
+
+    return None
+
+
+
+
+dateipfad = "C:\\Users\\kim0_\\Desktop\\Masterarbeit\\PruefplanValidierungsbauteil1_16.txt"
+
+print(getValuefromColumName(dateipfad, "nominal", 1))
+
