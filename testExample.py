926fa18d9d43077cf1c090461a466447269d1cd6
KimSkade <uuebm@student.kit.edu>
2023-09-21 08:32:33 +0200

    Anpassung Umwandlung in JSON für POST Request

---
+++ testExample.py
@@ -0,0 +1,29 @@
+import typing
+from aas2openapi import models
+
+
+class BillOfMaterial(models.Submodel):
+    components: typing.List[str]
+    result_check: bool
+
+
+class ProcessModel(models.Submodel):
+    processes: typing.List[str]
+    result_check: bool
+
+
+class Product(models.AAS):
+    bill_of_material: BillOfMaterial
+    process_model: typing.Optional[ProcessModel]
+
+
+example_product = Product(
+    id_="bc2119e48d0",
+    process_model=ProcessModel(
+        id_="a8cd10ed",
+        processes=["join", "screw"], result_check=False,
+    ),
+    bill_of_material=BillOfMaterial(
+        id_="a7cba3bcd", components=["stator", "rotor", "coil", "bearing"], result_check=True,
+    ),
+)
--- testUmwandlungJSON.py
+++ testUmwandlungJSON.py
@@ -14,23 +14,8 @@ data = Product(
     ),
 )
 
-data_dict = data.dict()
+data_dict= data.dict()
 # Wandele die Python-Daten in ein JSON-Format um
 json_data = json.dumps(data_dict)
 
-
-# Definiere den Befehl als Liste von Zeichenfolgen und verwende das JSON-Datenobjekt
-command = [
-    'curl',
-    '-X', 'POST',
-    '-H', 'Content-Type: application/json',
-    '-d', json_data,  # Verwende das JSON-Datenobjekt hier
-    'http://127.0.0.1:8000/Product/'
-]
-
-
-# Führe den Befehl aus
-try:
-    subprocess.run(command, check=True)
-except subprocess.CalledProcessError as e:
-    print(f"Fehler beim Ausführen des Befehls: {e}")
+print(json_data)
